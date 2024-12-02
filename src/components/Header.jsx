85be4e01e7fe62a9cb0c60dc2b33df54911eaa56
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-12-02 09:33:08 +0100

    v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { DarkThemeToggle } from "flowbite-react";
+import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
 
     return (
         <header>
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <nav className="bg-gray-100 dark:bg-gray-900 border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="https://flowbite.com" className="flex items-center">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9"
@@ -20,7 +21,7 @@ const Header = () => {
                                 className="inline-flex lg:hidden items-center p-2 ml-2 rounded-lg"
                                 aria-controls="mobile-menu" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
-                            x
+                            <IoMenu className="text-2xl" />
                         </button>
                     </div>
                     <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
@@ -28,28 +29,28 @@ const Header = () => {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                                   className="block py-2 pr-4 pl-3 text-color rounded lg:p-0"
                                    aria-current="page">Home</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded lg:p-0">Company</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded lg:p-0">Marketplace</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded lg:p-0">Features</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded lg:p-0">Team</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                   className="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded lg:p-0">Contact</a>
                             </li>
                         </ul>
                     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async'; 
 import { useTranslation } from 'react-i18next';   
 import Header from '../components/Header';
+import HeroSection from "../components/HeroSection";
+import ContentSection from "../components/ContentSection";
+import FeaturesSection from "../components/FeaturesSection.jsx";
+import ModalCreate from "../components/ModalCreate.jsx";
 
 const Home = () => {
   const { t } = useTranslation(); 
@@ -13,6 +17,10 @@ const Home = () => {
     <meta name='description' content={t('seo_description')} />
     </Helmet>
     <Header />
+    <HeroSection />
+    <ContentSection />
+    <FeaturesSection />
+      <ModalCreate show={false} />
     </>
   );
 };
