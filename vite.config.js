3d0602888cc536389d875d3db2e59475dce0038c
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-12-03 10:19:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,41 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss'
+import path from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: 'src',
-  build: {
-    chunkSizeWarningLimit: 1600,
-    outDir: '../dist',
-    emptyOutDir: true, 
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-        manualChunks: () => 'index.js',
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: 'src',
+    build: {
+      chunkSizeWarningLimit: 1600,
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+          manualChunks: () => 'index.js',
+        },
       },
     },
-  },
-  plugins: [react(),tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+    plugins: [react(), tailwindcss()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    define: {
+      'process.env': env, // Make env variables available in your app
+    },
+  };
+});
