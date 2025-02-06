94db75bd1e92f5b2a223e11e9cfb4828e5c2f6aa
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-02-06 21:20:29 +0100

    crud

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Helmet} from "react-helmet-async";
 import { useTranslation } from 'react-i18next';
 import {Link} from "react-router-dom";
+import CrudApi from "../components/CrudApi";
+
 const Sample = () => {
 
   const { t } = useTranslation();
@@ -16,11 +18,12 @@ const Sample = () => {
           {/* Hero Section */}
           <section className="text-center py-20 px-4">
               <h1 className="text-5xl md:text-7xl font-bold text-neon-blue">
-                  Sample Page
+                  CRUD Example
               </h1>
               <h2 className="text-2xl md:text-3xl mt-4">
-                  Customize this page
+                  Create, Read, Update & Delete
               </h2>
+              <CrudApi />
               <Link to="/">
                   <button className="mt-8 px-6 py-3 font-bold rounded-full transition duration-300">
                       Go Back
