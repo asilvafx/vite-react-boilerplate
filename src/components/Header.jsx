133c11fb6c052249a6d79a8d29448a2029a9fadc
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-02-16 22:15:51 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,17 +47,17 @@ const Header = () => {
                         <>
                         <div className="hidden md:flex gap-2 items-center">
                         <Link to="/dashboard">
-                            <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95">
+                            <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95 backdrop-blur-lg">
                                 <Home size="20" />
                             </button>
                         </Link>
                         <Link to="/create">
-                        <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95">
+                        <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95 backdrop-blur-lg">
                             <Plus size="20" />
                         </button>
                         </Link>
                         <Link to="/join">
-                        <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95">
+                        <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95 backdrop-blur-lg">
                             <Box size="20" />
                         </button>
                         </Link>
@@ -66,7 +66,7 @@ const Header = () => {
                             label=""
                             dismissOnClick={false}
                             renderTrigger={() => (
-                                <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95">
+                                <button className="cyber-button flex items-center bg-neutral-950 bg-opacity-95 backdrop-blur-lg">
                                     <Wallet size="20" className="me-2" />
                                     {shortenAddress(currentUser.web3_address)}
                                 </button>
