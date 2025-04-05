663b5f51d2fdd1d6e13412552e29cbda571cb3b7
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-04-06 00:41:38 +0200

    v1

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { CartProvider } from 'react-use-cart'
+import { Provider } from 'react-redux'
+
 import App from './App'
-import { CartProvider } from 'react-use-cart';
 
 import './lib/i18n';
 import './styles/index.css'
 import './styles/custom.css'
 
-import * as serviceWorker from './serviceWorker';
+import { store } from './providers/index.js'
+
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <Provider store={store}>
       <CartProvider>
           <App />
       </CartProvider>
+      </Provider>
   </React.StrictMode>
 );
 
