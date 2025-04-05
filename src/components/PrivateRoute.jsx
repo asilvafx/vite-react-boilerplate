168923a908964fb42a654fd868306185aca940e0
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-04-05 23:40:11 +0200

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import Cookies from "js-cookie";
+
+const PrivateRoute = () => {
+    // Call the function to get the actual login status
+
+    const isLoggedIn = Cookies.get('isLoggedIn');
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" />;
+    }
+
+    return <Outlet />;
+};
+
+export default PrivateRoute;
