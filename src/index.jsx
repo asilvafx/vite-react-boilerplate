78293f8c8d31268b6fdf68bcf4d93ce0d96e6b48
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-05-12 21:33:40 +0200

    v1

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
+import { CartProvider } from 'react-use-cart';
 import App from './App.jsx'
 import * as serviceWorker from './serviceWorker';
 import './lib/i18n';
@@ -8,7 +9,9 @@ import './styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <CartProvider>
+          <App />
+      </CartProvider>
   </StrictMode>,
 )
 
