b09149707e8f53dbf952d397c74af485113af797
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-07-16 21:20:47 +0200

    v1

--- src/components/Common/LanguageSelector.jsx
+++ src/components/Common/LanguageSelector.jsx
@@ -21,10 +21,39 @@ const LanguageSelector = () => {
 
     return (
         <Select
-            className="bg-white text-gray-800 cursor-pointer"
             value={LangOptions.find(opt => opt.value === i18n.language)}
             onChange={handleChange}
             options={LangOptions}
+            styles={{
+                control: (base, state) => ({
+                    ...base,
+                    backgroundColor: '#333',
+                    borderRadius: '0.3rem',
+                    borderColor: state.isFocused ? '#999' : '#ccc',
+                    boxShadow: 'none',
+                    '&:hover': {
+                        borderColor: '#aaa'
+                    }
+                }),
+                menu: (base) => ({
+                    ...base,
+                    backgroundColor: '#333',
+                }),
+                option: (base, state) => ({
+                    ...base,
+                    backgroundColor: state.isSelected
+                        ? '#444'
+                        : state.isFocused
+                            ? '#333'
+                            : '#333',
+                    color: '#f0f0f0',
+                    cursor: 'pointer',
+                }),
+                singleValue: (base) => ({
+                    ...base,
+                    color: '#f0f0f0',
+                }),
+            }}
         />
     );
 };
